4ee8eb477da2a5f41652e7e6bb11f5b213d2511c
davodhiambo <jesseomolo@yahoo.com>
2024-06-18 05:39:53 +0300

    files: create go.mod

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	url := "https://jsonplaceholder.typicode.com/todos/1/"
 
 	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data := string(bodyBytes)
+		fmt.Println(data)
+
+	}
 }
