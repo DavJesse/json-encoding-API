9a19fd033e3b1ac8b688ce02b9fef1f692629cc4
davodhiambo <jesseomolo@yahoo.com>
2024-06-23 22:23:36 +0300

    read and print out body

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 type Todo struct {
-	UserID    int    `json:"userId"`
-	ID        int    `json:"id"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
+	UserID    int    `json:"userId,omitempy"`
+	ID        int    `json:"id,omitempy"`
+	Title     string `json:"title,omitempy"`
+	Completed bool   `json:"completed,omitempy"`
 }
 
 func main() {
@@ -23,6 +24,13 @@ func main() {
 	}
 
 	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", string(body))
 
 	if response.StatusCode == http.StatusOK {
 		todoItem := Todo{}
